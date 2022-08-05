26f6aeb50b9b8e724cbed3f1c627835355b744ce
Marco Antonio Bustillos Quiroz <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2022-08-04 23:35:31 -0500

    Update HiWorld.py
    8/57

--- HiWorld.py
+++ HiWorld.py
@@ -1,5 +1,20 @@
 print("Hi World")
 print("I am Marco Antonio")
 number = 50.6
+uName = "Marco Antonio"
+uGMale = True
 print(number)
-print(type(number))
+print(type(uGMale))
+
+if (uGMale):
+  print(uName + " is Male")
+else:
+  print(uName + " is Female")
+# This is a comment
+'''
+This is a comment block
+'''
+n1=120
+n2=150
+suma=(n1+n2*5)/12
+print(suma)
