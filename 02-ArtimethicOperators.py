8f7e1c9738a139fb2f108fc288090cc990610064
MarAntBQ <github@marantbq.dev>
2024-06-21 23:08:47 -0500

    Practice Math

---
+++ 02-ArtimethicOperators.py
@@ -0,0 +1,13 @@
+n1 = 6
+n2 = 3
+
+sum = n1 + n2
+rest = n1 - n2
+multiplication = n1 * n2
+divition = n1 / n2
+
+print('Sum:', sum)
+print('Rest:', rest)
+print('Multiplication:', multiplication)
+## Make the result of the divition an integer
+print('Divition:', int(divition))
