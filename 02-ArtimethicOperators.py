10624648bdab5939a62df0f2d3e41ab28eccbebd
MarAntBQ <github@marantbq.dev>
2024-06-21 23:32:27 -0500

    feat: Add arithmetic operations and modulo calculation
    The code changes in this commit add arithmetic operations such as division, floor division, and exponentiation to the existing code. It also includes the calculation of the modulo operation. This update enhances the functionality of the script by providing more mathematical operations.
    
    Note: The recent user commits and repository commits do not provide any additional context for the commit message.

--- 02-ArtimethicOperators.py
+++ 02-ArtimethicOperators.py
@@ -5,9 +5,20 @@ sum = n1 + n2
 rest = n1 - n2
 multiplication = n1 * n2
 divition = n1 / n2
+entire_divition = n1 // n2
+potentiation = n1 ** n2
 
 print('Sum:', sum)
 print('Rest:', rest)
 print('Multiplication:', multiplication)
 ## Make the result of the divition an integer
-print('Divition:', int(divition))
+print('Divition:', divition)
+print('Entire Divition:', entire_divition)
+print('Potentiation:', potentiation)
+
+
+num1 = 9
+num2 = 3
+
+modulo = num1 % num2
+print('Modulo:', modulo)
