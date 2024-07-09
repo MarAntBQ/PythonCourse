af55ea35a7b6e153e0d61a30875128a4952fb783
MarAntBQ <github@marantbq.dev>
2024-07-08 23:54:41 -0500

    Finish first practice with a formula

---
+++ Exercises/01.py
@@ -0,0 +1,7 @@
+a = float(input('Please enter the value of a: '))
+b = float(input('Please enter the value of b: '))
+c = float(input('Please enter the value of c: '))
+
+r = ((c+5)*(b**2 - 3*a*c)*a**2) / (4*a)
+
+print(r)
