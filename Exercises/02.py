dd806eb08386e09f5ec6e5471907809dfac96e37
MarAntBQ <github@marantbq.dev>
2024-07-11 01:08:31 -0500

    Finish exercise # 2

---
+++ Exercises/02.py
@@ -0,0 +1,19 @@
+a = int(input("Insert the value of A: "))
+b = int(input("Insert the value of B: "))
+
+a,b=b,a
+
+# if (a > b):
+#     temp = b
+#     b = a
+#     a = temp
+# elif (a == b):
+#     temp = a
+# else:
+#     temp = a
+#     a = b
+#     b = temp
+
+
+print(f"A is finally {a}")
+print(f"B is finally {b}")
