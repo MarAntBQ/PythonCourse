4ff684dd196025de23a0af50a01e0dfcaa93da1d
MarAntBQ <github@marantbq.dev>
2024-08-05 23:45:11 -0500

    feat: Update constants and print statements in Python files

---
+++ 06-Constants.py
@@ -0,0 +1,11 @@
+# Declaring Constants in Python
+
+PI = 3.1416
+ADMIN_EMAIL = "Hire-Me@MarAntBQ.dev"
+
+
+print(PI)
+print(ADMIN_EMAIL)
+
+PI = 3.14159 # Even though we can change the value of a constant, it is not recommended to do so because with UPPERCASE letters we are telling other developers that this variable is a constant and should not be changed.
+print(PI)
---
+++ 07-Pi.py
@@ -0,0 +1,4 @@
+# Printing the value of Pi to the screen
+import math
+
+print(math.pi) # This is the value of Pi in the math module
