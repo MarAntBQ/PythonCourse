928e58326d408354b873bca58ea7eb41f08d9504
MarAntBQ <github@marantbq.dev>
2024-08-05 23:24:28 -0500

    feat: Add personal information to AboutMe program

---
+++ 02-AboutMe.py
@@ -0,0 +1,9 @@
+userName = "Marco Antonio"
+age = 28
+emailAddress = "Hire-Me@MarAntBQ.dev"
+webSite = "https://MarAntBQ.dev"
+country = "Ecuador"
+
+# Print the information
+# I had used the "\n" to make a new line
+print("Name:", userName, "\nAge:", age, "\nEmail:", emailAddress, "\nWebsite:", webSite, "\nCountry:", country)
