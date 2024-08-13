30a41125e809610c2518be9d963166001c92a033
MarAntBQ <github@marantbq.dev>
2024-08-12 22:48:17 -0500

    feat: Update strings and variables in Python program

---
+++ 08-Strings.py
@@ -0,0 +1,16 @@
+# Learn Strings
+
+string1 = "Hi Mundo"
+userName = "Marco Antonio"
+career = "Full Stack Developer"
+
+# Change Career
+career = "Backend Developer"
+
+# Multiple line
+string2 = '''This is a multiple line
+Example of a string in Python
+'''
+
+print(string2)
+
